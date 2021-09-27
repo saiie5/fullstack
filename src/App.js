8cf173bfbedd324c6a35290b340d71b01a65103d
saiie5 <56484744+saiie5@users.noreply.github.com>
2021-09-27 10:00:20 +0530

    added bacic react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+export default App;
